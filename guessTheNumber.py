3727c1a47d2ccc76aad1bbfa799bb82f5103d9d2
IuliiaLanchuk <lanchuk.iuliia@gmail.com>
2020-12-20 14:24:08 +0300

    GUI with tkinter

--- guessTheNumber.py
+++ guessTheNumber.py
@@ -1,4 +1,36 @@
 import random
+from tkinter import *
+
+
+#кнопка Начать игру
+def clicked():
+    lbl.configure(text="Добро пожаловать в числовую угадайку!\n\nЯ уже загадала число от 1 до 100.\n\n"
+                       "Попробуй угадать число.", padx="50", pady="8")
+    btn_start.destroy()
+    variant.grid(column=0, row=3)
+    btn_guess.grid(column=0, row=4, padx="20", pady="8.5")
+
+
+#кнопка Угадать
+def guessed():
+    value = variant.get()
+    if value:
+        print(value)
+    else:
+        print('Empty Entry')
+    print('кнопка угадать нажата')  # это выведется в консоли
+    if is_valid(value):
+        if int(value) < n:
+            lbl.configure(text="Ваше число меньше загаданного,\n попробуйте еще разок", padx="60", pady="18")
+        elif int(value) > n:
+            lbl.configure(text="Ваше число больше загаданного,\n попробуйте еще разок", padx="60", pady="18")
+        elif int(value) == n:
+            variant.destroy()
+            btn_guess.destroy()
+            lbl.configure(text='Вы угадали! Поздравляем!\n\nСпасибо, что играли в числовую угадайку.\nЕще увидимся...')
+    else:
+        lbl.configure(text='А может быть все-таки введем\n целое число от 1 до 100?', padx="80", pady="8")
+        print('А может быть все-таки введем целое число от 1 до 100?')
 
 
 # проверка, что введенное это цифры
@@ -9,31 +41,18 @@ def is_valid(num):
         return False
 
 
-def guess_the_number():
-    n = random.randint(1, 100)
-    print('Добро пожаловать в числовую угадайку!')
-    print('Попробуйте угадать число от 1 до 100')
-    print(n)
-    counter = 0
-    for i in range(100):
-        user_variant = input()
-        if is_valid(user_variant):
-            if int(user_variant) < n:
-                print('Ваше число меньше загаданного, попробуйте еще разок')
-                counter += 1
-                continue
-            elif int(user_variant) > n:
-                print('Ваше число больше загаданного, попробуйте еще разок')
-                counter += 1
-                continue
-            elif int(user_variant) == n:
-                counter += 1
-                print('Вы угадали c', counter, 'попытки! поздравляем!')
-                print('Спасибо, что играли в числовую угадайку. Еще увидимся...')
-                break
-        else:
-            print('А может быть все-таки введем целое число от 1 до 100?')
-            continue
-
-
-print(guess_the_number())
+window = Tk()
+window.title("Приложение Угадайка")
+window.config(bg='bisque2')
+window.geometry('350x250')
+lbl = Label(window, text="Привет,меня зовут Юля!\nСыграешь со мной в угадайку? Тогда жми на кнопку!",
+            font=("Arial Bold", 10), bg='bisque2', padx="10", pady="18")
+lbl.grid(column=0, row=0)
+btn_start = Button(window, text="Начать игру", bg="LightBlue1", fg="grey26", command=clicked)
+btn_start.grid(column=0, row=1)
+btn_guess = Button(window, text="Угадать", bg="LightBlue1", fg="grey26", command=guessed)
+variant = Entry(window)
+#здесь генерируется случайное число
+n = random.randint(1, 100)
+print('Рандом-', n) #в консоль выведется загаданное число
+window.mainloop()
